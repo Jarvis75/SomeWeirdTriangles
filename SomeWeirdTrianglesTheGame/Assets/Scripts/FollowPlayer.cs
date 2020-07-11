a9116c887b5cb67c5420fd6fec93a3b5c619f63b
Jarvis75 <florian.guillaumey@gmail.com>
2020-07-11 15:10:35 +0200

    HealthBar
    Functionnal healthbar which follow the player

---
+++ SomeWeirdTrianglesTheGame/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y+0.8f, player.position.z);
+    }
+}
---
+++ SomeWeirdTrianglesTheGame/Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image healthBar;
+    public static float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ChangeHealth(-0.1f);
+        }
+        healthBar.fillAmount = health;
+    }
+
+    public void ChangeHealth(float amount)
+    {
+        if (health > 0)
+        {
+            health += amount;
+        }
+    }
+}
